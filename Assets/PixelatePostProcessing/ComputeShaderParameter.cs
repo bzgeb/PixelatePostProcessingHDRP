5ba83ad4e57a1ca5b18601c6e395a7e14bce1647
Bronson Zgeb <bronson.zgeb@unity3d.com>
2021-07-30 22:31:50 -0400

    Added ComputeShaderParameter

---
+++ Assets/PixelatePostProcessing/ComputeShaderParameter.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[System.Serializable]
+public class ComputeShaderParameter : VolumeParameter<ComputeShader>
+{
+    public ComputeShaderParameter(ComputeShader value, bool overrideState = false)
+        : base(value, overrideState)
+    {
+    }
+}
--- Assets/PixelatePostProcessing/PixelateImageFilter.cs
+++ Assets/PixelatePostProcessing/PixelateImageFilter.cs
@@ -6,39 +6,29 @@ using System;
 [Serializable, VolumeComponentMenu("Post-processing/Custom/PixelateImageFilter")]
 public sealed class PixelateImageFilter : CustomPostProcessVolumeComponent, IPostProcessComponent
 {
-    [Tooltip("Controls the intensity of the effect.")]
     public ClampedIntParameter BlockSize = new ClampedIntParameter(5, 2, 20);
-    public BoolParameter ShowInSceneView = new BoolParameter(false, false);
-    public ComputeShader FilterComputeShader;
-
-    public bool IsActive() => FilterComputeShader != null;
+    public BoolParameter ShowInSceneView = new BoolParameter(false);
+    public ComputeShaderParameter FilterComputeShaderParameter = new ComputeShaderParameter(null);
     
+    public bool IsActive() => FilterComputeShaderParameter.value != null;
+
     public override bool visibleInSceneView => ShowInSceneView.value;
 
     public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
 
-    public override void Setup()
-    {
-        if (FilterComputeShader == null) 
-            FilterComputeShader = Resources.Load<ComputeShader>("Pixelate");
-    }
-
     public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
     {
-        var mainKernel = FilterComputeShader.FindKernel("Pixelate");
-        FilterComputeShader.GetKernelThreadGroupSizes(mainKernel, out uint xGroupSize, out uint yGroupSize, out _);
-        cmd.SetComputeTextureParam(FilterComputeShader, mainKernel, "_ImageFilterSource", source.nameID);
-        cmd.SetComputeTextureParam(FilterComputeShader, mainKernel, "_ImageFilterResult", destination.nameID);
-        cmd.SetComputeIntParam(FilterComputeShader, "_BlockSize", BlockSize.value);
-        cmd.SetComputeIntParam(FilterComputeShader, "_ResultWidth", destination.rt.width);
-        cmd.SetComputeIntParam(FilterComputeShader, "_ResultHeight", destination.rt.height);
-        cmd.DispatchCompute(FilterComputeShader, mainKernel,
+        var filterComputeShader = FilterComputeShaderParameter.value;
+        var mainKernel = filterComputeShader.FindKernel("Pixelate");
+        filterComputeShader.GetKernelThreadGroupSizes(mainKernel, out uint xGroupSize, out uint yGroupSize, out _);
+        cmd.SetComputeTextureParam(filterComputeShader, mainKernel, "_ImageFilterSource", source.nameID);
+        cmd.SetComputeTextureParam(filterComputeShader, mainKernel, "_ImageFilterResult", destination.nameID);
+        cmd.SetComputeIntParam(filterComputeShader, "_BlockSize", BlockSize.value);
+        cmd.SetComputeIntParam(filterComputeShader, "_ResultWidth", destination.rt.width);
+        cmd.SetComputeIntParam(filterComputeShader, "_ResultHeight", destination.rt.height);
+        cmd.DispatchCompute(filterComputeShader, mainKernel,
             Mathf.CeilToInt(destination.rt.width / (float) BlockSize.value / xGroupSize),
             Mathf.CeilToInt(destination.rt.height / (float) BlockSize.value / yGroupSize),
             1);
-    }
-
-    public override void Cleanup()
-    {
     }
 }
